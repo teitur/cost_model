c2a80c25fc4c31426972449bfa61da223de7e83e
teitur <teitur.arnarson@gmail.com>
2019-06-15 03:13:57 +0200

    Added google chart.

--- server.js
+++ server.js
@@ -2,12 +2,27 @@
 const express = require('express'),
       server = express();
 
+server.engine('html', require('ejs').renderFile);
+
 server.set('port', process.env.PORT || 8080);
 
 //Basic routes
+/*
 server.get('/', (request,response)=>{
    response.send('Home page');
 });
+*/
+
+server.get('/', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+
+  var count = 10
+  var dbDetails = {type: 'type', url: 'hej://'}
+  res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+});
+
+
 
 server.get('/about',(request,response)=>{
    response.send('About page');
@@ -22,5 +37,5 @@ server.use((request,response)=>{
 
 //Binding to a port
 server.listen(8080, ()=>{
-  console.log('Express server started at port 3000');
+  console.log('Express server started at port 8080');
 });
